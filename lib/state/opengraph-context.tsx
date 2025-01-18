bd375246d61c9240b102479e63306f4991f1d94d
emmathedeveloper2 <emmadavid063@gmail.com>
2025-01-18 15:43:41 +0100

    Bug Fix

--- lib/state/opengraph-context.tsx
+++ lib/state/opengraph-context.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import type { Opengraph } from "~/lib/type";
+import { Opengraph } from "../type";
 
 export type OpenGraphContextType = {
     openGraphData: Opengraph;
