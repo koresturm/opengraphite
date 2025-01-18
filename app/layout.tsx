94c5fc177cfacea54227383cf186077c0aac30b9
emmathedeveloper2 <emmadavid063@gmail.com>
2025-01-18 17:02:57 +0100

    Added Twitter opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,20 @@ export const metadata: Metadata = {
       },
     ],
   },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Opengraphite",
+    description: "A simple tool to generate opengraph tags for your website.",
+    images: [
+      {
+        url: "https://opengraphite.vercel.app/opengraph.png",
+        width: 1200,
+        height: 630,
+        alt: "Opengraphite",
+      },
+    ],
+  }
 };
 
 export default function RootLayout({
