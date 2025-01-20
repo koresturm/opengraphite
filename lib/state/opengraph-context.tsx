0254bfa1a0b699aa9598c47c06e5d49ccf459db2
emmathedeveloper2 <emmadavid063@gmail.com>
2025-01-20 13:22:30 +0100

    Bug fixes

--- lib/state/opengraph-context.tsx
+++ lib/state/opengraph-context.tsx
@@ -7,14 +7,14 @@ export type OpenGraphContextType = {
 };
 
 export const OpenGraphContext = createContext<OpenGraphContextType>({
-    openGraphData: { type: "", title: "", description: "", url: "" },
+    openGraphData: { type: "", title: "", description: "", domain: "" },
     setOpenGraphData: () => { },
 });
 
 export const useOpenGraphContext = () => useContext<OpenGraphContextType>(OpenGraphContext);
 
 export const OpenGraphProvider = ({ children }: { children: React.ReactNode }) => {
-    const [openGraphData, setOpenGraphData] = useState<Opengraph>({ type: "", title: "", description: "", url: "" });
+    const [openGraphData, setOpenGraphData] = useState<Opengraph>({ type: "", title: "", description: "", domain: "" });
 
     return (
         <OpenGraphContext.Provider value={{ openGraphData, setOpenGraphData }}>
